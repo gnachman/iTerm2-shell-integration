3533d93b9eb5cbd360c50f69c02fed900bd896d6
Joe Block <jpb@unixorn.net>
2023-06-04 23:58:14 -0600

    Source the helper functions, too
    Signed-off-by: Joe Block <jpb@unixorn.net>

--- iterm2-shell-integration.plugin.zsh
+++ iterm2-shell-integration.plugin.zsh
@@ -1,4 +1,8 @@
 # Automagically add the utilities directory to $PATH
 
-PLUGIN_BIN="$(dirname $0)/bin"
+# Add utilities to our path
+PLUGIN_BIN="$(dirname $0)/utilities"
 export PATH=${PATH}:${PLUGIN_BIN}
+
+# And source the helper functions, too.
+source "${0:h}/shell_integration/zsh"
