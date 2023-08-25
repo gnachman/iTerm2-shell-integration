5349ca66c2af03f9546bb4d2464e12277e598db8
Joe Block <jpb@unixorn.net>
2023-08-25 17:16:01 -0600

    Use better path manipulation, update plugin file name
    @nunico mentioned a better way to massage `$PATH`, use it instead.
    
    Rename iterm2-shell-integration.plugin.zsh -> iTerm2-shell-integration.plugin.zsh
    per @nunico's comments about issues with zimfw and potentially other plugin managers.
    
    Signed-off-by: Joe Block <jpb@unixorn.net>

--- iTerm2-shell-integration.plugin.zsh
+++ iTerm2-shell-integration.plugin.zsh
@@ -1,8 +1,7 @@
 # Automagically add the utilities directory to $PATH
 
 # Add utilities to our path
-PLUGIN_BIN="$(dirname $0)/utilities"
-export PATH=${PATH}:${PLUGIN_BIN}
+path+=("${0:h}/utilities")
 
 # And source the helper functions, too.
 source "${0:h}/shell_integration/zsh"
